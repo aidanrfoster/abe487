5fba4eb042282d6a7e29df551467e7109452a9f3
Aidan Richard Foster <aidanrfoster@login2.cm.cluster>
2017-09-20 21:52:02 -0700

    complete homework/hello

--- problems/hello/hello-arg2.sh
+++ problems/hello/hello-arg2.sh
@@ -1,3 +1,16 @@
+
 #!/bin/bash
 
-echo "OK"
+set -u
+
+if [[ $# -lt 1 ]] || [[ $# -gt 2 ]]; then
+	printf "Usage: %s GREETING [NAME]\n" "$(basename "$0")"
+	exit 1
+fi
+
+GREETING=$1
+NAME=${2:-Stranger}
+
+printf "%s,%s\n" "$GREETING" " $NAME!"
+
+
