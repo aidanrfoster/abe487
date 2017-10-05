10ff4fe72a697b6ba6fdda4cc1d7d79d043767d1
Aidan Richard Foster <aidanrfoster@login2.cm.cluster>
2017-10-05 10:30:49 -0700

    works well enough

--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
-"""count the vowels in a word"""
 
-print('OK')
+import sys
+
+if len(sys.argv) <2:
+	print('Usage:', sys.argv[0], 'STRING')
+	sys.exit(1)
+else:
+	num_vowels = 0
+	for char in sys.argv[1]:
+		if char in 'aeiou':
+			num_vowels = num_vowels + 1
+	if num_vowels == 1:
+		print('There is',num_vowels,'vowel in','"{}."'.format(sys.argv[1]))
+	else:
+		num_vowels = 0
+		for char in sys.argv[1]:
+			if char in 'aeiou':
+				num_vowels = num_vowels + 1
+		if num_vowels >=2:
+			print('There are',num_vowels,'vowels in','"{}."'.format(sys.argv[1]))
+		else:
+			for char in sys.argv[1]:
+				if char in 'aeiou':
+					num_vowels = num_vowels + 1
+			if num_vowels == 0:
+				print('There are',num_vowels,'vowels in','"{}."'.format(sys.argv[1]))
+
