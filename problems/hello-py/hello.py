a2bbecfc034c351b99a11bbc12907d7924c95fa8
Aidan Richard Foster <aidanrfoster@login2.cm.cluster>
2017-10-10 22:59:08 -0700

    adding hello-py homework

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,17 +1,4 @@
 #!/usr/bin/env python3
+"""say hello to the arguments"""
 
-import sys
-import os
-
-if len(sys.argv) <2:
-	print('Usage:', sys.argv[0], 'NAME [NAME2 ...]')
-	sys.exit(1)
-elif len(sys.argv) ==2:
-	print('Hello to the', len(sys.argv[1:]),'of you:','{}!'.format(sys.argv[1]))
-elif len(sys.argv) ==3:
-        print('Hello to the {} of you: {} and {}!' .format(len(sys.argv[1:]), sys.argv[1], sys.argv[2]))
-elif len(sys.argv) >=3:
-	print('Hello to the {} of you: {}, and {}!' .format(len(sys.argv[1:]), ', '.join(sys.argv[1:-1]), sys.argv[-1]))
-
-
-
+print('OK')
