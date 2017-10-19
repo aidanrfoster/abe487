a23a4adff35ecc3ddb45804ef3ce2372b7af2ead
Aidan Richard Foster <aidanrfoster@login2.cm.cluster>
2017-10-18 21:27:28 -0700

    kmer hw

--- problems/kmer_counter/kmer_counter.py
+++ problems/kmer_counter/kmer_counter.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python3
 
-print('OK')
+import os
+import sys
+from collections import defaultdict
+from collections import Counter
+
+args = sys.argv[1:]
+
+if len(args) != 2:
+	print('Usage: kmer_counter.py LEN STR')
+	exit(1)
+
+if len(args) == 2:
+	if not args[0].isdigit():
+		print('Kmer size "{}" is not a number' .format(args[0]))
+		sys.exit(1)
+	if int(args[0]) <= 0:
+		print('Kmer size "{}" must be > 0' .format(args[0]))
+		sys.exit(1)
+	if int(args[0]) > len(args[1]):
+		print('There are no {}-mers in "{}"' .format(args[0],args[1]))
+		sys.exit(1)
+
+seq = args[1]
+kmer_len = int(args[0])
+kmer = len(seq) - int(args[0]) +1
+inp = defaultdict(int)
+
+for bases in range(kmer):
+	kmer_range = seq[bases:bases+kmer_len]
+	inp[kmer_range] += 1
+
+print(seq)
+for key, value in sorted(inp.items()):
+	print(key, value)
